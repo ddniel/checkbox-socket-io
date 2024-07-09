854dcbeac0e1a0139ab0e310cc947955b51a0b85
DannyBec <ddnielbec@outlook.com>
2024-07-09 09:59:27 -0400

    Assignment done

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("Hi");
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("checked", (id, checked) => {
+    io.emit("checked", id, checked);
+  });
+});
+
+server.listen(3000, () => {
+  console.log(`app running at http://localhost:3000`);
 });
